15972d7790b76e05701d32b8417b068c3dc7d9e9
Tom <hogiahuyml2@gmail.com>
2026-06-22 10:51:17 +0700

    Fix contact section icons and links
    - Instagram icon: change from 📷 to @ (more appropriate for social account)
    - Zalo link: change to tel:+84906123456 (need to update with actual phone)
    - Avatar image: add onerror fallback to hide if load fails
    - Both Instagram links now use consistent @ icon

--- js/main.js
+++ js/main.js
@@ -56,15 +56,15 @@
       '<div class="footer-contact">' +
         '<div class="fc-inner">' +
           '<div class="fc-founder">' +
-            '<img src="images/founder.jpg" alt="Gia Huy" class="fc-avatar">' +
+            '<img src="images/founder.jpg" alt="Gia Huy" class="fc-avatar" onerror="this.style.display=\'none\'">' +
             '<p class="fc-name">Gia Huy</p>' +
           '</div>' +
           '<div class="fc-links">' +
-            '<a href="https://zalo.me/giahuy02" target="_blank" class="fc-link fc-zalo" title="Zalo">💬</a>' +
+            '<a href="tel:+84906123456" class="fc-link fc-zalo" title="Zalo">💬</a>' +
             '<a href="https://www.facebook.com/giahuy.tom02/" target="_blank" class="fc-link fc-fb" title="Facebook (Gia Huy)">f</a>' +
             '<a href="https://www.facebook.com/EnglishwTom" target="_blank" class="fc-link fc-fb" title="Facebook (English With Tom)">f</a>' +
-            '<a href="https://www.instagram.com/hg_huyy/" target="_blank" class="fc-link fc-ig" title="Instagram (Personal)">📷</a>' +
-            '<a href="https://www.instagram.com/eng.with.tom/" target="_blank" class="fc-link fc-ig" title="Instagram (Page)">📷</a>' +
+            '<a href="https://www.instagram.com/hg_huyy/" target="_blank" class="fc-link fc-ig" title="Instagram (Personal)">@</a>' +
+            '<a href="https://www.instagram.com/eng.with.tom/" target="_blank" class="fc-link fc-ig" title="Instagram (Page)">@</a>' +
           '</div>' +
         '</div>' +
       '</div>' +
